d022f7c1bdca4b059d4ec652922c94061524ca91
Sonja Parsell <hello@sparsell.com>
2020-04-03 09:41:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  hash.collect do |key, amt|
-    if amt <= 8
-      puts key
+
+ min_value = 100 #set a min value to start comparing
+ key_for_min_value = nil
+
+  hash.each do |key, value|
+    if min_value >= value
+      min_value = value
+      #key_min_value = key #update min val and key when find a lower one through iteration
     else
+      
     end
-end
+    return key
+  end
 end
