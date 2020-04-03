fcf9d9b67b68242e2ee1dda3a4cfeb70d44a279a
Sonja Parsell <hello@sparsell.com>
2020-04-03 09:41:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+  hash.collect do |key, amt|
+    if amt <= 8
+      puts key
+    else
+    end
+end
 end
